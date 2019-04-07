ebbc073ab1fa670e0ac9c06f14c6649ba5d99e00
Dustin Joynt <dustinjoynt@gmail.com>
2019-04-07 16:08:57 -0600

    added methods to city class and updated tests

--- Objects/src/components/cityComp/city.js
+++ Objects/src/components/cityComp/city.js
@@ -5,6 +5,41 @@ class City{
 		this.longitude = longitude
 		this.population = population
 	}
+
+	show () {
+		return(
+		`${this.name} ${this.latitude} ${this.longitude} ${this.population}`
+		)
+	}
+
+	movedIn (amt) {
+		return(
+			this.population = this.population + amt
+			)
+	}
+
+	movedOut (amt) {
+		return(
+			this.population = this.population - amt)
+	}
+
+	howBig () {
+		if(this.population <= 100){
+			return('Hamlet')
+		}
+		if(this.population < 1000){
+			return('Village')
+		}
+		if(this.population < 20000){
+			return('Town')
+		}
+		if(this.population < 10000){
+			return('Large Town')
+		}
+		else{
+			return('City')
+		}
+	}
 }
 
 export default City
--- Objects/src/components/cityComp/city.test.js
+++ Objects/src/components/cityComp/city.test.js
@@ -1,11 +1,20 @@
 import City from './city.js'
 
 test('works', () =>{
-	const obj = new City('Calgary', 51, -114, 1267344)
+	const obj = new City('Calgary', 51, -114, 10000)
 	//test constructor
 	expect(obj.name).toBe('Calgary')
 	expect(obj.latitude).toBe(51)
 	expect(obj.longitude).toBe(-114)
-	expect(obj.population).toBe(1267344)
-	
+	expect(obj.population).toBe(10000)
+	//test show 
+	expect(obj.show()).toBe('Calgary 51 -114 10000')
+	//test moved in
+	expect(obj.movedIn(100)).toBe(10100)
+	//test moved out
+	expect(obj.movedOut(50)).toBe(10050)
+	//test how big
+	expect(obj.howBig()).toBe('Town')
+	obj.movedIn(100000)
+	expect(obj.howBig()).toBe('City')
 })
